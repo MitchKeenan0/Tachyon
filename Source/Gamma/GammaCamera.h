3fc97edc0b1c075c16bd52d46bb09947dbb8821a
MitchKeenan <mitchkeenan0@gmail.com>
2018-04-09 12:45:16 -0400

    Added bare singleton camera class "GammaCamera"

---
+++ Source/Gamma/GammaCamera.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "GammaCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMMA_API AGammaCamera : public ACameraActor
+{
+	GENERATED_BODY()
+	
+	
+
+	
+	
+};
